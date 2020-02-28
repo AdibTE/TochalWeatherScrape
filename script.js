afd216eaf2ac118f7bfd82747472d225cfce2be0
adibte <adibte@hotmail.com>
2020-02-29 02:51:18 +0330

    v 1.0.0

---
+++ script.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+const cheerio = require('cheerio');
+const Temp = require('./Temp');
+
+app.get('/:range', (req, res) => {
+    request(`https://www.snow-forecast.com/resorts/Tochal/forecasts/feed/${req.params.range}/m`, (err, response, html) => {
+        if (!err && response.statusCode == 200) {
+            const $ = cheerio.load(html);
+            const botData = $('#table-temp').find('td').first();
+            let temp = botData.text().replace(/\D+/gm, '').split('');
+            let outlook = $('#table-outlook').find('td').first().find('img').attr('alt');
+            let data = {
+                min: temp[1],
+                max: temp[0],
+                outlook
+            };
+            res.json(data)
+        }
+    });
+});
+
+// Listen port
+let port = process.env.PORT || 10;
+app.listen(port, () => {
+    console.log(`[ LISTENING ON PORT ${port} ]`);
+});
