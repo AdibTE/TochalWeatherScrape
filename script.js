2c47999f21e2b4b356cc326fde9a23baba5fcd33
Adib M.Rohani <adibte@hotmail.com>
2020-07-06 15:39:07 +0430

    implement data for 5 days

--- script.js
+++ script.js
@@ -4,27 +4,36 @@ const request = require('request');
 const cheerio = require('cheerio');
 const cors = require('cors');
 
-app.use(cors())
+app.use(cors());
 
 app.get('/:range', (req, res) => {
-    request(`https://www.snow-forecast.com/resorts/Tochal/forecasts/feed/${req.params.range}/m`, (err, response, html) => {
-        if (!err && response.statusCode == 200) {
-            const $ = cheerio.load(html);
-            let outlook = $('#table-outlook').find('td').first().find('img').attr('alt');
-            const botData = $('#table-temp').find('td').first();
-            let temp = botData.text().split('|');
-            let data = {
-                min: parseInt(temp[1]),
-                max: parseInt(temp[0]),
-                outlook
-            };
-            res.json(data)
-        }
-    });
+	request(
+		`https://www.snow-forecast.com/resorts/Tochal/forecasts/feed/${req.params.range}/m`,
+		(err, response, html) => {
+			if (!err && response.statusCode == 200) {
+				const $ = cheerio.load(html);
+				let allData = [];
+				for (i = 0; i < 5; i++) {
+                    let day = $('#table-day').find('td').eq(i).children().text();
+					let outlook = $('#table-outlook').find('td').eq(i).find('img').attr('alt');
+					let Data = $('#table-temp').find('td').eq(i);
+					let temp = Data.text().split('|');
+					let data = {
+						min: parseInt(temp[1]),
+						max: parseInt(temp[0]),
+                        outlook,
+                        day
+					};
+					allData.push(data)
+				}
+				res.json(allData);
+			}
+		}
+	);
 });
 
 // Listen port
 let port = process.env.PORT || 10;
 app.listen(port, () => {
-    console.log(`[ LISTENING ON PORT ${port} ]`);
+	console.log(`[ LISTENING ON PORT ${port} ]`);
 });
